7cf4087f805d730b2e07c279dcf2cac6e9afd33d
Radu Vasile Catalin <raducata11@yahoo.com>
2019-04-22 16:18:34 +0300

    Version 0.1

---
+++ Doodle Like Game/Assets/EnemyController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public LayerMask layer;
+    private bool isGrounded;
+
+    private void Update()
+    {
+        Ray2D ray2D = new Ray2D(transform.position, new Vector2(1, -1));
+        Debug.DrawRay(ray2D.origin, ray2D.direction);
+        RaycastHit2D raycastHit = Physics2D.Raycast(ray2D.origin, ray2D.direction, 1, layer.value);
+        Debug.Log(raycastHit.collider.name);
+    }
+}
---
+++ Doodle Like Game/Assets/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb2D;
+    public float movementSpeed;
+    public float jumpForce;
+    private float horizontalMovement;
+    private bool jump = false;
+
+    public float jumpDelta = 0.5F;
+    private float nextJump = 0.5F;
+    private float myTime = 0.0F;
+
+    void Awake()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        horizontalMovement = Input.GetAxisRaw("Horizontal");
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jump = true;
+        }
+        myTime = myTime + Time.deltaTime;
+    }
+
+    private void FixedUpdate()
+    {
+        rb2D.AddForce(new Vector2(horizontalMovement * movementSpeed, 0));
+        if (jump && myTime > nextJump)
+        {
+            jump = false;
+            rb2D.AddForce(new Vector2(0, jumpForce));
+            nextJump = myTime + jumpDelta;
+            nextJump = nextJump - myTime;
+            myTime = 0.0F;
+        }
+    }
+
+}
